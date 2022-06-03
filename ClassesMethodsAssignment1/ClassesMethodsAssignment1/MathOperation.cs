fd8e300975556c75943484260d6c12006d24fc8a
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-02 21:39:32 -0300

    Classes and Methods Assignment 1

---
+++ ClassesMethodsAssignment1/ClassesMethodsAssignment1/MathOperation.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMethodsAssignment1
+{
+    public class MathOperation
+    {
+        //Takes in an integer parameter and adds 100 to the value
+        //Returns sum as integer
+        public int Addition(int value)
+        {
+            int result = value + 100;
+
+            return result;
+        }
+
+        //Takes in a decimal parameter and adds 5.5 to the value
+        //Returns sum as integer
+        public int Addition(decimal value)
+        {
+            decimal result = value + 5.5m;
+            int intResult = Convert.ToInt32(result);
+
+            return intResult;
+        }
+
+        //Takes in a string parameter and gets the length of that string as an int
+        //String length is added to 20
+        //Returns sum as integer
+        public int Addition(string value)
+        {
+            int result = value.Length + 20;
+
+            return result;
+        }
+    }
+}
