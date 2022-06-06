1bf15dc2589ba22b96b09159da70aca1b0d4be3b
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-06 16:00:44 -0300

    Classes and Methods Assignment 3

---
+++ ClassesMethodsAssignment3/ClassesMethodsAssignment3/MathOperation.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesMethodsAssignment3
+{
+    public class MathOperation
+    {
+        //A void method that does not need to return anything
+        //Multiply numOne by 2
+        //Display numTwo in console
+        public void MathOperationAndDisplay(int numOne, int numTwo)
+        {
+            int result = numOne * 2;
+            Console.WriteLine(numOne + " * 2 = " + result);
+            Console.WriteLine("The second number was " + numTwo);
+        }
+    }
+}
