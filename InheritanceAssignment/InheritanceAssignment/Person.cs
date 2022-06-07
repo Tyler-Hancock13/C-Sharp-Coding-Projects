42b5c34095279ccb8486278bf79a862597160148
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-06 23:37:20 -0300

    Inheritance Assignment

---
+++ InheritanceAssignment/InheritanceAssignment/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+
+        //Displays the FirstName and LastName attributes in the console
+        public void SayName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+            Console.ReadLine();
+        }
+    }
+}
