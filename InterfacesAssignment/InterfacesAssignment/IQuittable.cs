9fccc5ac5da7ba3a6383719029bf571ac2af6cdd
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-10 00:03:09 -0300

    Interfaces Assignment

---
+++ InterfacesAssignment/InterfacesAssignment/IQuittable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    interface IQuittable
+    {
+        //Quit method that returns void and has one boolean parameter named isEmployed
+        void Quit(bool isEmployed);
+    }
+}
---
+++ InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfacesAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create new employee object
+            Employee employee = new Employee();
+            employee.FirstName = "Sample";
+            employee.LastName = "Student";
+            employee.isEmployed = true;
+
+            employee.SayName();
+
+            employee.Quit(employee.isEmployed);
+            employee.isEmployed = false;
+
+            Console.ReadLine();
+        }
+    }
+}
