5ce68669e97733d71dd9d892ccc76e88f127a01f
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-13 01:38:46 -0300

    Additional Features Assignment

---
+++ AdditionalFeaturesAssignment/AdditionalFeaturesAssignment/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdditionalFeaturesAssignment
+{
+    public class Employee
+    {
+        public Employee(string firstName, string lastName) : this(firstName, lastName, "No department")
+        {
+        }
+        public Employee(string firstName, string lastName, string department)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Department = department;
+        }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+    }
+}
