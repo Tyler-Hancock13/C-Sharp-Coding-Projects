84742a6665331fc6dbc1a7d713530315a22e4e30
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-18 23:56:23 -0300

    Fixed Polymorphism error

--- InterfacesAssignment/InterfacesAssignment/Program.cs
+++ InterfacesAssignment/InterfacesAssignment/Program.cs
@@ -11,15 +11,18 @@ namespace InterfacesAssignment
         static void Main(string[] args)
         {
             //Create new employee object
-            Employee employee = new Employee();
-            employee.FirstName = "Sample";
-            employee.LastName = "Student";
-            employee.isEmployed = true;
+            //Employee employee = new Employee();
+            //IQuittable iEmployee = employee;
+            //employee.FirstName = "Sample";
+            //employee.LastName = "Student";
+            //employee.isEmployed = true;
 
-            employee.SayName();
+            //employee.SayName();
 
-            employee.Quit(employee.isEmployed);
-            employee.isEmployed = false;
+            //employee.Quit(employee.isEmployed);
+            //employee.isEmployed = false;
+            IQuittable iemp = new Employee() { FirstName = "Tyler", LastName = "Hancock", isEmployed = true };
+            iemp.Quit(true);
 
             Console.ReadLine();
         }
