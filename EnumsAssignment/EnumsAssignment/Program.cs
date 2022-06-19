afc3dea4dd7e32f60820b5e25b0a29e060ae8901
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-18 21:38:29 -0300

    Enums Assignment

---
+++ EnumsAssignment/EnumsAssignment/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnumsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter the current day of the week.");
+                string userValue = Console.ReadLine();
+                DaysOfTheWeek day = (DaysOfTheWeek)Enum.Parse(typeof(DaysOfTheWeek), userValue);
+                Console.WriteLine("\nYou entered " + day.ToString());
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Please enter an actual day of the week.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
