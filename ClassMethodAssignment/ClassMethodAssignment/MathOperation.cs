ecbdb79f17a9f4bca1a5e83c58ee356f46aab388
Tyler Hancock <tyler.hancock13@hotmail.com>
2022-06-18 22:38:03 -0300

    Class Method Assignment

---
+++ ClassMethodAssignment/ClassMethodAssignment/MathOperation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public static class MathOperation
+    {
+        public static void Divide(int num)
+        {
+            int result = num / 2;
+
+            Console.WriteLine("{0} divided by 2 is {1}", num, result);
+        }
+
+        public static int Multiply(int num)
+        {
+            return num * 2;
+        }
+
+        public static int Multiply(int numOne, int numTwo)
+        {
+            return numOne * numTwo;
+        }
+    }
+}
